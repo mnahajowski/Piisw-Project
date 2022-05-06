aa19096668608d522bc6569ac31f52aacdd82f5e
mwdmwd <pmwpmw@protonmail.ch>
2022-05-06 04:26:26 +0200

    backend: Add user controller and service

---
+++ backend/src/main/java/pl/transport/backend/controllers/UserController.java
@@ -0,0 +1,25 @@
+package pl.transport.backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.transport.backend.data.users.User;
+import pl.transport.backend.services.UserService;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+	private final UserService userService;
+
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping("/")
+	public Iterable<User> getAll() {
+		return userService.getAll();
+	}
+}
---
+++ backend/src/main/java/pl/transport/backend/services/UserService.java
@@ -0,0 +1,21 @@
+package pl.transport.backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.transport.backend.data.users.User;
+import pl.transport.backend.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	private UserRepository userRepository;
+
+	@Autowired
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public Iterable<User> getAll() {
+		return userRepository.findAll();
+	}
+}
