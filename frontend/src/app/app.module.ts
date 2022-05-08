94a2525a8b63beb5ad340d2ba5fe947bafb534bf
Marcin Nahajowski <immortalmar0@gmail.com>
2022-05-08 14:25:37 +0200

    frontend: update imported modules in app.modules.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {TicketsModule} from "./tickets/tickets.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    TicketsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
