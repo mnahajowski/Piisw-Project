accfdaff2d085e00fad5e221165e63bbfa62a143
Marcin Nahajowski <immortalmar0@gmail.com>
2022-05-28 18:15:31 +0200

    frontend: improve view of ticket list component

--- frontend/src/app/tickets/components/ticket-list/ticket-list.component.ts
+++ frontend/src/app/tickets/components/ticket-list/ticket-list.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import{ Ticket } from '../../models/ticket'
 import {ActivatedRoute} from "@angular/router";
 import {Location} from "@angular/common";
+import {Assortment} from "../../models/assortment";
+import {TicketType} from "../../models/ticket-type";
+import {SingleTicket} from "../../models/single-ticket";
+import {LongTimeTicket} from "../../models/long-time-ticket";
+import {TimeTicket} from "../../models/time-ticket";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-ticket-list',
@@ -10,19 +16,121 @@ import {Location} from "@angular/common";
 })
 export class TicketListComponent implements OnInit{
 
-  tickets: Ticket[] | undefined;
+  assortment: Assortment | undefined;
+  tickets: TicketType[] = [];
+  singleTickets: SingleTicket[] = [];
+  longTimeTickets: LongTimeTicket[] = [];
+  timeTickets: TimeTicket[] = []
+  ticketTypes: String[] = ['.TimeTicket', '.LongTimeTicket', '.SingleTicket' ]
+  // singleTicketForm: FormGroup;
+  // timeTicketForm: FormGroup;
+  // longTimeTicketForm: FormGroup;
 
-  constructor(private readonly route: ActivatedRoute) {
-
+  constructor(private readonly route: ActivatedRoute, private modalService: NgbModal) {
+    // this.singleTicketForm = this.fb.group({
+    //   busNumber: ['',Validators.required, Validators.pattern("^[0-9]*$")]
+    // });
   }
   ngOnInit() {
-    this.tickets = this.route.snapshot.data['tickets'];
-    console.log('--- book ---');
-    console.log(this.tickets);
+    this.assortment = this.route.snapshot.data['tickets'];
 
+    console.log(this.tickets);
+    console.log(this.assortment?.ticketTypes);
+    // @ts-ignore
+    this.tickets = this.assortment?.ticketTypes;
+    // @ts-ignore
+    this.tickets.forEach((ticket: TicketType) => {
+      if (ticket.type === '.SingleTicket') {
+        this.singleTickets.push(ticket);
+      } else if (ticket.type === '.LongTimeTicket') {
+        this.longTimeTickets.push(ticket);
+      } else if (ticket.type === '.TimeTicket') {
+        this.timeTickets.push(ticket);
+      }
+
+    })
+    console.log(this.singleTickets);
+    console.log(this.timeTickets);
+    console.log(this.longTimeTickets);
+    localStorage.removeItem("time");
+    localStorage.removeItem("prize");
+    localStorage.removeItem("discount");
+
+  }
+
+  getTicketType(ticket: TicketType) : Boolean{
+    console.log(ticket.type)
+    return true;
+  }
+
+  findDiscounted(tickets: TicketType[] | undefined, shouldBeDiscounted: boolean): TicketType[] {
+    // @ts-ignore
+    return tickets.filter(ticket => ticket.discounted === shouldBeDiscounted)
+  }
+
+  findSpecificTickets(tickets: TicketType[], ticketType: String): TicketType[]{
+    return tickets.filter(ticket => ticket.type === ticketType)
   }
   // ngOnInit(): void {
   //   console.log(this.tickets)
   // }
+
+  getPolishNameType(ticketType: String, isMulti:boolean) {
+    if (ticketType === '.SingleTicket') {
+      if (isMulti)
+        return 'Bilety jednorazowe';
+      return 'Bilet jednorazowy';
+    }
+    else if (ticketType === '.LongTimeTicket') {
+      if (isMulti)
+        return 'Bilety długototerminowe';
+      return 'Bilet długototerminowy';
+    }
+    else if (ticketType === '.TimeTicket') {
+      if (isMulti)
+        return 'Bilety czasowe';
+      return 'Bilet czasowy';
+    }
+    else
+      return null;
+  }
+
+  getPolishDiscount(isDiscount: boolean) {
+    if (isDiscount)
+      return 'Ulgowy';
+    else
+      return 'Normalny';
+  }
+
+  getTicketTypeFormComponent(ticketType: String) {
+    if (ticketType === '.SingleTicket') {
+      return "/buySingleTicket";
+    }
+    else if (ticketType === '.TimeTicket')
+      return "/buyTimeTicket";
+    else if (ticketType === '.LongTimeTicket')
+      return "/buyLongTimeTicket";
+    return null;
+  }
+
+  saveData(ticketType: String, content: [Number, String, Number]) {
+    console.log(content);
+    // @ts-ignore
+    localStorage.setItem("prize", JSON.parse(content[0]));
+    // @ts-ignore
+    localStorage.setItem("discount", content[1]);
+    // @ts-ignore
+    localStorage.setItem("time", JSON.parse(content[2]));
+  }
+
+  getTime(ticket: TicketType) {
+    if (ticket.type === '.TimeTicket' || ticket.type === '.LongTimeTicket') {
+      return ticket.validitySeconds;
+    } else {
+      return 1;
+    }
+  }
+
+
 
 }
