5cb585ca95f2d2480fc47a37f48905679397f8cd
Marcin Nahajowski <immortalmar0@gmail.com>
2022-05-28 18:08:35 +0200

    frontend: add assortment and ticket-type models

---
+++ frontend/src/app/tickets/models/assortment.ts
@@ -0,0 +1,5 @@
+import {TicketType} from "./ticket-type";
+
+export interface Assortment {
+  ticketTypes: TicketType[]
+}
---
+++ frontend/src/app/tickets/models/ticket-type.ts
@@ -0,0 +1,7 @@
+export interface TicketType{
+  type: String;
+  name : String;
+  price : number;
+  discounted : boolean;
+  validitySeconds: Number;
+}
