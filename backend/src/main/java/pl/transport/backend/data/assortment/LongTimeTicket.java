de8b331cf9f528d65798dc93e8bd0dba3406b89b
mwdmwd <pmwpmw@protonmail.ch>
2022-05-30 09:32:58 +0200

    backend: Disallow buying LongTimeTickets in the past

--- backend/src/main/java/pl/transport/backend/data/assortment/LongTimeTicket.java
+++ backend/src/main/java/pl/transport/backend/data/assortment/LongTimeTicket.java
@@ -8,7 +8,7 @@ import pl.transport.backend.data.tickets.Ticket;
 import javax.persistence.Entity;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
 import java.util.TimeZone;
 
 @Entity(name = "LongTimeTicketType")
@@ -25,9 +25,19 @@ public class LongTimeTicket extends TicketType {
 	}
 
 	@Override
+	// TODO Optional<Ticket>
 	public Ticket create(Long startTime) {
-		// TODO disallow creation with start time too far in the past or future (- 5min?)
 		var time = LocalDateTime.ofInstant(Instant.ofEpochSecond(startTime), TimeZone.getDefault().toZoneId());
+		var day = time.truncatedTo(ChronoUnit.DAYS);
+		var now = LocalDateTime.now();
+		var today = now.truncatedTo(ChronoUnit.DAYS);
+
+		if (day.isBefore(today)) {
+			throw new IllegalArgumentException("Can't create a long time ticket in the past");
+		}
+		else if (day.isEqual(today)) {
+			time = now;
+		}
 		return new pl.transport.backend.data.tickets.LongTimeTicket(time, getValiditySeconds());
 	}
 
