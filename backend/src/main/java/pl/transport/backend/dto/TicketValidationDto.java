99f8be469ee738eab943d7e948e1497a390c5631
mwdmwd <pmwpmw@protonmail.ch>
2022-05-31 18:08:22 +0200

    backend: Fix deserialization of TicketValidationDto

--- backend/src/main/java/pl/transport/backend/dto/TicketValidationDto.java
+++ backend/src/main/java/pl/transport/backend/dto/TicketValidationDto.java
@@ -1,8 +1,11 @@
 package pl.transport.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Value
+@NoArgsConstructor(force = true)
 public class TicketValidationDto {
 
     long ticketId;
