4810cb30b1771cf24c2f1b8fd6dcd93fc033d1fe
Marcin Nahajowski <immortalmar0@gmail.com>
2022-06-01 14:30:41 +0200

    frontend: update ticket type model, add few params

--- frontend/src/app/tickets/models/ticket-type.ts
+++ frontend/src/app/tickets/models/ticket-type.ts
@@ -1,7 +1,13 @@
+import {ValidityStatus} from "./validity-status";
+
 export interface TicketType {
   type: string;
   name: string;
   price: number;
   discounted: boolean;
   validitySeconds: number;
+  validityStatus: ValidityStatus;
+  startTime: number;
+  validationTime: number;
+  validationRouteNumber: string;
 }
